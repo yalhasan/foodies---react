bd5b7393b6ba0b7a7e9f0a20770511ed733f4f08
yalhasan <yalhasan13@gmail.com>
2023-11-09 10:43:44 +0300

    havent done much

---
+++ foodies/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./pages/Navbar";
+
+function App() {
+  // const [user, setUser] = useState(false);
+
+  // useEffect(() => {
+  //   setUser(checkToken());
+  // }, []);
+
+  return (
+    //heelo
+    <Navbar />
+  );
+}
+
+export default App;
---
+++ foodies/src/pages/Navbar.js
@@ -0,0 +1,113 @@
+// import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-368F8B">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link to="/">
+              <span className="font-semibold text-xl text-white">
+                Our Notes
+              </span>
+            </Link>
+          </div>
+          <div className="block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <NavLink
+                to="/"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/notes"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Notes
+              </NavLink>
+              <NavLink
+                to="/users"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Users
+              </NavLink>
+
+              <>
+                <NavLink
+                  to="/login"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Login
+                </NavLink>
+                <NavLink
+                  to="/register"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Register
+                </NavLink>
+              </>
+              <div>
+                <div class="flex md:order-2">
+                  <button
+                    type="button"
+                    data-collapse-toggle="navbar-search"
+                    aria-controls="navbar-search"
+                    aria-expanded="false"
+                    class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+                  >
+                    <svg
+                      class="w-5 h-5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      />
+                    </svg>
+                    <span class="sr-only">Search</span>
+                  </button>
+                  <div class="relative hidden md:block">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg
+                        class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                        />
+                      </svg>
+                      <span class="sr-only">Search icon</span>
+                    </div>
+                    <input
+                      type="text"
+                      id="search-navbar"
+                      class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Search..."
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
